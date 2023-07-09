66b33d8cc7d52f375ff16dc0a75e9f5318e70a2a
dilane-goune <dkgoune@gmail.com>
2023-07-09 20:04:51 +0100

    Updated messaging

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
 // const SERVER_URL = "http://192.168.43.34:3000";
-const SERVER_URL = "http://roomy-finder-dev-2.ap-1.evennode.com";
-// const SERVER_URL = "http://roomy-finder-evennode.ap-1.evennode.com";
+// const SERVER_URL = "http://roomy-finder-dev-2.ap-1.evennode.com";
+const SERVER_URL = "http://roomy-finder-evennode.ap-1.evennode.com";
 
 const API_URL = "$SERVER_URL/api/v1";
 
@@ -12,6 +12,7 @@ final priceRegex = RegExp(r'(^\d*\.?\d{0,2})');
 const DYNAMIC_LINK_URL = "https://roomyfinder.page.link";
 
 const PRIVACY_POLICY_LINK = "$SERVER_URL/public/privacy-policy";
+
 const TERMS_AND_CONDITIONS_LINK = "$SERVER_URL/public/terms-and-conditions";
 
 const FEED_BACK_LINK = "$SERVER_URL/public/feedback";
